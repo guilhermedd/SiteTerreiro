9d795bdb6ac29ca28fc402d5d3fda418ff90f872
Guilherme Diel <guiddiel@gmail.com>
2025-01-31 17:26:04 -0300

    content secuity

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,25 +1,8 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.content_security_policy do |policy|
+  # Permite o carregamento de estilos, scripts e fontes do CDN do Bootstrap
+  policy.style_src :self, 'https://cdn.jsdelivr.net'
+  policy.script_src :self, 'https://cdn.jsdelivr.net'
+  policy.font_src :self, 'https://cdn.jsdelivr.net'
+  policy.img_src :self, 'https://cdn.jsdelivr.net'
 
-# Define an application-wide content security policy.
-# See the Securing Rails Applications Guide for more information:
-# https://guides.rubyonrails.org/security.html#content-security-policy-header
-
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+end
