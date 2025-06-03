bda99bc7d1a05199ef4ff62394fabd2f63a6ec97
cesardesouza <cesarsouza.2002@icloud.com>
2025-06-03 15:31:58 -0300

    refactor: removing useless functions

--- app/models/gira.rb
+++ app/models/gira.rb
@@ -3,27 +3,24 @@ class Gira < ApplicationRecord
 
   validates :event_date, :name, :type_of_gira, :description, presence: true
 
-  def month_name
-    months = {
-      1 => "Janeiro",
-      2 => "Fevereiro",
-      3 => "Março",
-      4 => "Abril",
-      5 => "Maio",
-      6 => "Junho",
-      7 => "Julho",
-      8 => "Agosto",
-      9 => "Setembro",
-      10 => "Outubro",
-      11 => "Novembro",
-      12 => "Dezembro",
-    }
-    months[event_date.month]
-  end
+  # def month_name
+  #   months = {
+  #     1 => "Janeiro",
+  #     2 => "Fevereiro",
+  #     3 => "Março",
+  #     4 => "Abril",
+  #     5 => "Maio",
+  #     6 => "Junho",
+  #     7 => "Julho",
+  #     8 => "Agosto",
+  #     9 => "Setembro",
+  #     10 => "Outubro",
+  #     11 => "Novembro",
+  #     12 => "Dezembro",
+  #   }
+  #   months[event_date.month]
+  # end
 
-  def start_time
-    event_date
-  end
 
   def get_day
     "%.2d/%.2d/%d" % [event_date.day, event_date.month, event_date.year]
